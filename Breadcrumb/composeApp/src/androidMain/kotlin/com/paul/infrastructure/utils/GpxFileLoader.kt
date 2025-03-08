e74b73406c4d29f24219b1d5190b5e84283fd866
Paul Johnston <pauljohnston2009@live.com>
2025-03-08 20:11:03 +1000

    store gpx contents for history locally so we can read the files at a later date

--- Breadcrumb/composeApp/src/androidMain/kotlin/com/paul/infrastructure/utils/GpxFileLoader.kt
+++ Breadcrumb/composeApp/src/androidMain/kotlin/com/paul/infrastructure/utils/GpxFileLoader.kt
@@ -2,6 +2,7 @@ package com.paul.infrastructure.utils
 
 import android.content.Context
 import android.net.Uri
+import android.provider.OpenableColumns
 import androidx.activity.result.ActivityResultLauncher
 import androidx.compose.material.SnackbarHostState
 import com.paul.infrastructure.protocol.Point
@@ -14,15 +15,20 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withContext
 import java.io.ByteArrayInputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 import java.io.InputStream
 import java.nio.charset.StandardCharsets
+import java.util.UUID
 import kotlin.coroutines.resumeWithException
 import io.ticofab.androidgpxparser.parser.domain.Point as GpxPoint;
 
 
 data class GpxFile(
     val uri: String,
-    val gpx: Gpx
+    val gpx: Gpx,
+    val filename: String,
 ) {
     suspend fun toRoute(snackbarHostState: SnackbarHostState): Route? {
         // prefer track, then routes (world topo app creation is a route, most other gpx's i've seen are a single track)
@@ -93,7 +99,7 @@ data class GpxFile(
             }
         }
 
-        return uri
+        return filename
     }
 }
 
@@ -111,21 +117,87 @@ class GpxFileLoader(private val context: Context) {
         getContentLauncher = _getContentLauncher
     }
 
-    suspend fun searchForGpxFile(): GpxFile {
-        val uri_str = searchForGpxFileUri()
-        return GpxFile(uri_str, loadFileContents(Uri.parse(uri_str)))
+    private fun writeStringToFile(filename: String, data: String, append: Boolean = false): String? {
+        return try {
+            // Get the internal storage directory and create the file
+            val file = File(context.filesDir, filename)
+
+            FileOutputStream(file, append).use { outputStream ->
+                outputStream.write(data.toByteArray())
+            }
+            file.toURI().toString()
+        } catch (e: IOException) {
+            e.printStackTrace() // Log the exception for debugging
+            null
+        }
     }
 
-    suspend fun loadGpxFile(uri: Uri): GpxFile {
-        return GpxFile(uri.toString(), loadFileContents(uri))
+    fun generateRandomFilename(fileExtension: String? = null): String {
+        val uuid = UUID.randomUUID().toString()
+        return if (fileExtension != null && fileExtension.isNotEmpty()) {
+            "$uuid.$fileExtension"
+        } else {
+            uuid
+        }
     }
 
-    suspend fun loadGpxFile(uri: String): GpxFile {
-        return loadGpxFile(Uri.parse(uri))
+    fun getFilenameFromUri(uri: Uri): String? {
+        if (uri.scheme == "file") {
+            return uri.lastPathSegment
+        }
+
+        if (uri.scheme != "content") {
+            return null // Not a content URI
+        }
+
+        val contentResolver = context.contentResolver
+        val cursor = contentResolver.query(uri, null, null, null, null)
+
+        cursor?.use { // Ensures cursor is closed after use
+            if (it.moveToFirst()) {
+                val displayNameColumnIndex = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                if (displayNameColumnIndex != -1) {
+                    return it.getString(displayNameColumnIndex)
+                }
+            }
+        }
+        return null
+    }
+
+    suspend fun loadGpxFile(uri: Uri, firstTimeSeenFile: Boolean): GpxFile {
+        return withContext(Dispatchers.IO) {
+            var stream: InputStream? = null
+            try {
+                stream = context.contentResolver.openInputStream(uri)!!
+                val fileContents = loadFileContents(stream)
+                val filename = getFilenameFromUri(uri) ?: generateRandomFilename(".gpx")
+                var writtenFile: String? = uri.toString()
+                if (firstTimeSeenFile) {
+                    writtenFile = writeStringToFile(filename, fileContents.second)
+                    if (writtenFile == null) {
+                        throw SecurityException("failed to save file")
+                    }
+                }
+
+                return@withContext GpxFile(writtenFile!!, fileContents.first, filename)
+            } finally {
+                stream?.close()
+            }
+        }
+    }
+
+    suspend fun loadGpxFile(uri: String, firstTimeSeenFile: Boolean): GpxFile {
+        return loadGpxFile(Uri.parse(uri), firstTimeSeenFile)
     }
 
     suspend fun loadGpxFromInputStream(stream: InputStream): GpxFile {
-        return GpxFile(stream.toString(), parser.parse(stream))
+        val fileContents = loadFileContents(stream)
+        // todo give the option of naming this, or editing it when saving name to history
+        val filename = generateRandomFilename("googleroute.gpx")
+        val writtenFile: String = writeStringToFile(filename, fileContents.second)
+            ?: throw SecurityException("failed to save file")
+
+        return GpxFile(writtenFile, fileContents.first, filename)
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
@@ -148,20 +220,17 @@ class GpxFileLoader(private val context: Context) {
         getContentLauncher!!.launch(arrayOf("*"))
     }
 
-    private suspend fun loadFileContents(uri: Uri): Gpx {
+    private suspend fun loadFileContents(stream: InputStream): Pair<Gpx, String> {
         // see TracksBrowserActivity.displayImportTracksDialog in wormnav
         var streamContents = withContext(Dispatchers.IO) {
-            val stream = context.contentResolver.openInputStream(uri)
-            val ret = stream!!.readAllBytes().decodeToString()
-            stream.close()
-            ret
+            stream.readAllBytes().decodeToString()
         }
         // world topo map app track creation includes <time></time> sections with no value, this breaks the
         // parse, as it expects a time
         streamContents = streamContents.replace("<time></time>", "");
         val streamContentsAsStream: InputStream =
             ByteArrayInputStream(streamContents.toByteArray(StandardCharsets.UTF_8))
-        return parser.parse(streamContentsAsStream)
+        return Pair(parser.parse(streamContentsAsStream), streamContents)
     }
 
     fun fileLoaded(uri: Uri?) {
