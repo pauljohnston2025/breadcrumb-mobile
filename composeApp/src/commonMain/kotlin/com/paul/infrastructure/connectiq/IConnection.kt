46c646d8aee5d3865108de832bb2172e131ad7e3
Paul Johnston <pauljohnston2009@live.com>
2026-02-19 19:39:08 +1000

    Support new app glance

--- composeApp/src/commonMain/kotlin/com/paul/infrastructure/connectiq/IConnection.kt
+++ composeApp/src/commonMain/kotlin/com/paul/infrastructure/connectiq/IConnection.kt
@@ -31,6 +31,7 @@ abstract class IConnection {
 
         val BREADCRUMB_DATAFIELD_ID = "20edd04a-9fdc-4291-b061-f49d5699394d"
         val BREADCRUMB_APP_ID = "fa3e1362-11b0-4420-90cb-9ac14591bf68"
+        val BREADCRUMB_APP_GLANCE_ID = "54ea0621-918f-40ec-864e-d3fc7eb6ecbf"
         val LIGHT_WEIGHT_BREADCRUMB_DATAFIELD_ID = "65f96e86-28e7-4c92-9c5e-1bf7954bde41"
         val ULTRA_LIGHT_BREADCRUMB_DATAFIELD_ID = "ef62306c-4693-48e2-8f6b-beee5db9a2b8"
         val defaultConnectIqAppId = BREADCRUMB_DATAFIELD_ID // default to breadcrumb datafields (the original)
@@ -42,6 +43,7 @@ abstract class IConnection {
         val availableConnectIqApps = listOf(
             ConnectIqApp("BreadcrumbDataField", BREADCRUMB_DATAFIELD_ID),
             ConnectIqApp("BreadcrumbApp", BREADCRUMB_APP_ID),
+            ConnectIqApp("BreadcrumbAppGlance", BREADCRUMB_APP_GLANCE_ID),
             ConnectIqApp("LWBreadcrumbDataField", LIGHT_WEIGHT_BREADCRUMB_DATAFIELD_ID),
             ConnectIqApp("ULBreadcrumbDataField", ULTRA_LIGHT_BREADCRUMB_DATAFIELD_ID),
         )
